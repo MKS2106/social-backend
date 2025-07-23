ba8253f06baee84bf1310a1ed9ed3ebae817b4e5
Manasa <manasa.7m@gmail.com>
2025-07-23 10:34:09 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import db from "./config/connection.js";
+import cors from 'cors'
+import usersRouter from './routes/users.js'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({origin: 'http://localhost:5173'}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("api/users", usersRouter)
+
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
